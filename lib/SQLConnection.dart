acfd084ec646de7466cab865a9870f26485a7fe5
Amine Fajry <91888128+AmineFjr@users.noreply.github.com>
2024-09-02 16:00:58 +0200

    Update SQLConnection.dart

--- lib/SQLConnection.dart
+++ lib/SQLConnection.dart
@@ -5,7 +5,7 @@ class SQLConnection {
 
   connect() async  {
     conn = await MySqlConnection.connect(ConnectionSettings(
-        host: 'srv1015.hstgr.io', user: 'u986651684_admin', password: 'esgiLYON2024', db: 'u986651684_exploreplaces'
+        host: '', user: '', password: '', db: ''
     ));
 
     return conn;
